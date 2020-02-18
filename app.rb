f44af545fab7ba1233f99429baf3b02a62ed9a7d
Dave Winniford <dwinniford@gmail.com>
2020-02-18 22:20:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
     # @word5 = params[:word5]
     # "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
       @arr = []
-    binding.pry 
+    
     params.each do |k,v| 
       if k.to_s.match?("word")
         @arr << v
