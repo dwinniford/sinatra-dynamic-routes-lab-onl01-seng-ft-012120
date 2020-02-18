eabd34922920d0e904769d0de919eed1cf8c981a
Dave Winniford <dwinniford@gmail.com>
2020-02-18 22:00:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,10 +20,13 @@ class App < Sinatra::Base
   end 
     
   get '/say/:word1/:word2/:word3/:word4/:word5'
-    @word1 = params[:word1]
-    @word2 = params[:word2]
-    @word3 = params[:word3]
-    @word4 = params[:word4]
-    @word5 = params[:word5]
-
+    # @word1 = params[:word1]
+    # @word2 = params[:word2]
+    # @word3 = params[:word3]
+    # @word4 = params[:word4]
+    # @word5 = params[:word5]
+    # word1 word2 word3 word4 word5
+    params.each |k,v| do 
+      if k.to_s.include?("word")
+        
 end
